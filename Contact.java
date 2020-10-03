100096a65f1147fd1ce1e8c363a2be118e5d3c55
SwapnaneelBhuiya <43994683+SwapnaneelBhuiya@users.noreply.github.com>
2020-10-03 11:58:46 +0530

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -8,69 +8,69 @@ public class Contact {
     public static String phone_number;
     public static String email;
 
-        public static String getFirst_name() {
-            return first_name;
-        }
+    public  String getFirst_name() {
+        return first_name;
+    }
 
-        public static String getLast_name() {
-            return last_name;
-        }
+    public  String getLast_name() {
+        return last_name;
+    }
 
-        public static String getAddress() {
-            return address;
-        }
+    public  String getAddress() {
+        return address;
+    }
 
-        public static String getCity() {
-            return city;
-        }
+    public  String getCity() {
+        return city;
+    }
 
-        public static String getState() {
-            return state;
-        }
+    public  String getState() {
+        return state;
+    }
 
-        public static String getZip() {
-            return zip;
-        }
+    public  String getZip() {
+        return zip;
+    }
 
-        public static String getPhone_number() {
-            return phone_number;
-        }
+    public  String getPhone_number() {
+        return phone_number;
+    }
 
-        public static String getEmail() {
-            return email;
-        }
+    public  String getEmail() {
+        return email;
+    }
 
-        public static String setFirst_name(String a) {
-            first_name = a;
-        }
+    public  void setFirst_name(String a) {
+        first_name = a;
+    }
 
-        public static String setLast_name(String a) {
-            last_name = a;
-        }
+    public  void setLast_name(String a) {
+        last_name = a;
+    }
 
-        public static String setAddress(String a) {
-            address = a;
-        }
+    public  void setAddress(String a) {
+        address = a;
+    }
 
-        public static String setCity(String a) {
-            city = a;
-        }
+    public  void setCity(String a) {
+        city = a;
+    }
 
-        public static String setState(String a) {
-            state = a;
-        }
+    public  void setState(String a) {
+        state = a;
+    }
 
-        public static String setZip(String a) {
-            zip = a;
-        }
+    public  void setZip(String a) {
+        zip = a;
+    }
 
-        public static String setPhone_number(String a) {
-            phone_number = a;
-        }
+    public  void setPhone_number(String a) {
+        phone_number = a;
+    }
 
-        public static String setEmail(String a) {
-            email = a;
-        }
+    public  void setEmail(String a) {
+        email = a;
+    }
 
     public Contact(String a, String b, String c, String d, String e, String f, String g, String h) {
         first_name = a;
