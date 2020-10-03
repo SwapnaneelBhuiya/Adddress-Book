9c50fbb0ed39b7bc0f8a0aa66d01bfa1721afceb
SwapnaneelBhuiya <43994683+SwapnaneelBhuiya@users.noreply.github.com>
2020-10-03 11:57:22 +0530

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -5,31 +5,18 @@ import java.util.Map.Entry;
 public class Dictionary extends AddressBookMain
 {
     public static HashMap<String, ArrayList<Contact>> dict=new HashMap<String, ArrayList<Contact>>();
-    public static HashMap<String, String> dict_by_city=new HashMap<String,String>();
-    public static HashMap<String, String> dict_by_state=new HashMap<String,String>();
-
-	
-	  public void add_by_city_and_state() 
-	  { 
-		  for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
-		  {
-			  ArrayList<Contact> ar=ee.getValue();
-		  for (Contact i : ar) 
-		  { 
-			  dict_by_city.put(i.getCity(),i.getFirst_name() + " " + i.getLast_name()); 
-			  dict_by_state.put(i.getState(),i.getFirst_name() + " " + i.getLast_name()); 
-			  } 
-		  } 
-	  }
-	 
+	  
         public void search_by_city(String city)
         {
             int con=0;
             System.out.println("People living in the city "+city+" are");
-  		    for(Entry<String, String> ee:dict_by_city.entrySet())
+  		    for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
             {
-                if(ee.getKey().equals(city))
-                {con++;System.out.println(ee.getValue());}
+                for(Contact i:ee.getValue())
+                {
+                	if(i.getCity().equals(city))
+                {con++;System.out.println(i.getFirst_name()+" "+i.getLast_name());}
+                }
             }
             System.out.println("Number of contact person by city= "+con);
         }
@@ -38,11 +25,13 @@ public class Dictionary extends AddressBookMain
 
             int con=0;
             System.out.println("People living in the state "+state+" are");
-  		    for(Entry<String, String> ee:dict_by_state.entrySet())
+            for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
             {
-                if(ee.getKey().equals(state))
-                {con++;System.out.println(ee.getValue());}
-            }
+                for(Contact i:ee.getValue())
+                {
+                	if(i.getState().equals(state))
+                {con++;System.out.println(i.getFirst_name()+" "+i.getLast_name());}}
+                }
             System.out.println("Number of contact person by state= "+con);
         }
         public void contact_details()
