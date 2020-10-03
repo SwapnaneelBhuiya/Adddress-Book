317c330ff6781bfac77921bb8a1ae841e034760e
SwapnaneelBhuiya <43994683+SwapnaneelBhuiya@users.noreply.github.com>
2020-10-03 10:59:13 +0530

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,69 +1,72 @@
+package com.practice.main;
+
 import java.util.*;
+import java.util.Map.Entry;
 public class Dictionary extends AddressBookMain
 {
     public static HashMap<String, ArrayList<Contact>> dict=new HashMap<String, ArrayList<Contact>>();
-    public static HashMap<String, String> dict_by_city;
-    public static HashMap<String, String> dict_by_state;
-    public void add_by_city_and_state() {
-        Iterator it = dict.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry cont = (Map.Entry) it.next();
-            for (Contact i : cont.getValue()) {
-                dict_by_city.put(i.getCity(), i.getFirst_name() + " " + i.getLast_name());
-                dict_by_state.put(i.getState(), i.getFirst_name() + " " + i.getLast_name());
-            }
-        }
-    }
+    public static HashMap<String, String> dict_by_city=new HashMap<String,String>();
+    public static HashMap<String, String> dict_by_state=new HashMap<String,String>();
+
+	
+	  public void add_by_city_and_state() 
+	  { 
+		  for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
+		  {
+			  ArrayList<Contact> ar=ee.getValue();
+		  for (Contact i : ar) 
+		  { 
+			  dict_by_city.put(i.getCity(),i.getFirst_name() + " " + i.getLast_name()); 
+			  dict_by_state.put(i.getState(),i.getFirst_name() + " " + i.getLast_name()); 
+			  } 
+		  } 
+	  }
+	 
         public void search_by_city(String city)
         {
             int con=0;
             System.out.println("People living in the city "+city+" are");
-            Iterator it = dict_by_city.entrySet().iterator();
-            while(it.hasNext())
+  		    for(Entry<String, String> ee:dict_by_city.entrySet())
             {
-                Map.Entry c=(Map.Entry)it.next();
-                if(c.getKey().equals(city))
-                {con++;System.out.println(c.getValue());}
+                if(ee.getKey().equals(city))
+                {con++;System.out.println(ee.getValue());}
             }
             System.out.println("Number of contact person by city= "+con);
         }
         public void search_by_state(String state)
         {
+
             int con=0;
             System.out.println("People living in the state "+state+" are");
-            Iterator it = dict_by_state.entrySet().iterator();
-            while(it.hasNext())
+  		    for(Entry<String, String> ee:dict_by_state.entrySet())
             {
-                Map.Entry c=(Map.Entry)it.next();
-                if(c.getKey().equals(state))
-                {con++;System.out.println(c.getValue());}
+                if(ee.getKey().equals(state))
+                {con++;System.out.println(ee.getValue());}
             }
             System.out.println("Number of contact person by state= "+con);
         }
         public void contact_details()
         {
-            Iterator it = dict.entrySet().iterator();
             System.out.println("Contact Namewise Details");
-            while(it.hasNext())
+  		  for(Entry<String, ArrayList<Contact>> ee:dict.entrySet())
             {
-                Map.Entry cont = (Map.Entry)it.next();
-                String names=cont.getKey();
-                ArrayList<Contact> ar=cont.getValue();
+                String names=ee.getKey();
+                ArrayList<Contact> ar=ee.getValue();
                 System.out.println("The name is: "+names);
-                for(int i=0;i<ar.size();i++)
+                for(Contact i:ar)
                 {
-                    System.out.println(ar.getFirst_name());
-                    System.out.println(ar.getLast_name());
-                    System.out.println(ar.getAddress());
-                    System.out.println(ar.getCity());
-                    System.out.println(ar.getState());
-                    System.out.println(ar.getZip());
-                    System.out.println(ar.getPhone_number());
-                    System.out.println(ar.getEmail());
+                    System.out.println(i.getFirst_name());
+                    System.out.println(i.getLast_name());
+                    System.out.println(i.getAddress());
+                    System.out.println(i.getCity());
+                    System.out.println(i.getState());
+                    System.out.println(i.getZip());
+                    System.out.println(i.getPhone_number());
+                    System.out.println(i.getEmail());
                 }
             }
         }
-        public static void add_dict(ArrayList<Contact> ar)
+        public void add_dict(ArrayList<Contact> ar)
         {
             Scanner sc=new Scanner(System.in);
             System.out.println("Enter the name of the Address book");
